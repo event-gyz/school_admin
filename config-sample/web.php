406cb6e346ca161115aaaa65eccc367f784934de
关雅卓 <yazhuo_guan@eventown.com>
2018-06-19 15:31:57 +0800

    12

--- config-sample/web.php
+++ config-sample/web.php
@@ -80,17 +80,15 @@ $config = [
     'defaultRoute'=>'/site/index'
 ];
 
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];
+$config['bootstrap'][] = 'debug';
+$config['modules']['debug'] = [
+    'class' => 'yii\debug\Module',
+];
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
-}
+$config['bootstrap'][] = 'gii';
+$config['modules']['gii'] = [
+    'class' => 'yii\gii\Module',
+    'allowedIPs' => ['*']
+];
 
 return $config;
